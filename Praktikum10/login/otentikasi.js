9a339b7ad1c30cd4bcce085caa6f04f7319734e0
syavitri <114562857+syavitri@users.noreply.github.com>
2022-12-07 14:57:43 +0700

    Add files via upload

---
+++ Praktikum10/login/otentikasi.js
@@ -0,0 +1,14 @@
+function login() {
+    event.preventDefault();
+
+    let uname = document.getElementById("username").value;
+    let pwd = document.getElementById("password").value;
+
+    if (uname == "ahmad2017" && pwd == "integrity"){location.replace("sukses.html");
+        alert("Login Berhasil");
+    } else{
+        alert("Login gagal");
+    }
+
+
+}
